5b79cc74673ee5f6980ba62983070227f2a3f1f6
Dominic Carrington <carringtondominic@gmail.com>
2023-12-01 21:01:45 +0000

    [2023][1.2] Complete

--- 2023/1.2/task.py
+++ 2023/1.2/task.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-numbers = {
+numerals = {
     "one": 1,
     "two": 2,
     "three": 3,
@@ -10,55 +10,42 @@ numbers = {
     "six": 6,
     "seven": 7,
     "eight": 8,
-    "nine": 9
+    "nine": 9,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
 }
 
-numbersRegex = re.compile("(" + "|".join(numbers.keys()) + ")")
-
-def replaceNumericWithInteger(line: str) -> str:
-    ## Attempt 1: This fails since it searches for the numbers in order
-    ## However, this breaks for cases like "twone". This code turns it
-    ## into "tw1" however the correct solution is "2ne"
-    ## Answer: 55584
-    # for num in numbers.keys():
-    #     line = line.replace(num, str(numbers[num]))
-    # return line
-
-    ## Attempt 2: This uses regex to convert the strings into the numeric
-    ## equivalents however this again has given an answer too low.
-    ## Answer: 56001
-    return numbersRegex.sub(lambda s: str(numbers[s.group()]), line)
-
-def convertToInteger(str):
-    if len(str) > 1:
-        return None
-    
-    try:
-        i = int(str)
-        
-        return i if i > 0 else None
-    except ValueError:
-        return None
-
-def convertLineToNumber(line):
-    line = replaceNumericWithInteger(line)
-
+def convertLineToNumber(line: str) -> int:
     i = 0
     first_number = None
     last_number = None
-    while i < len(line) and (first_number is None or last_number is None):
-        if first_number is None:
-            first_number = convertToInteger(line[i])
+    while i < len(line) and first_number is None:
+        for n in numerals.keys():
+            if line[i:(i+len(n))] == n:
+                first_number = numerals[line[i:(i+len(n))]]
+                break
+        i += 1
 
-        if last_number is None:
-            last_number = convertToInteger(line[len(line) - i - 1])
-
+    i = 0
+    line = line[::-1]
+    while i < len(line) and last_number is None:
+        for n in numerals.keys():
+            if line[i:(i+len(n))] == n[::-1]:
+                last_number = numerals[line[i:(i+len(n))][::-1]]
+                break
         i += 1
     
     if first_number is None or last_number is None:
-        raise RuntimeError("[{}] first_number ({}) or last_number ({}) are none".format(line.strip(), first_number, last_number))
-    
-    return first_number * 10 + last_number
+        raise RuntimeError("[{}] first_number ({}) or last_number ({}) are none".format(line, first_number, last_number))
+
+    return (first_number * 10 + last_number)
 
 def main():
     sigma = 0
--- 2023/1.2/test.py
+++ 2023/1.2/test.py
@@ -1,47 +1,6 @@
 import task
 import unittest
 
-class ConvertToIntegerTestCase(unittest.TestCase):
-    def test_1(self):
-        self.assertEqual(task.convertToInteger("1"), 1)
-    def test_2(self):
-        self.assertEqual(task.convertToInteger("2"), 2)
-    def test_3(self):
-        self.assertEqual(task.convertToInteger("3"), 3)
-    def test_4(self):
-        self.assertEqual(task.convertToInteger("4"), 4)
-    def test_5(self):
-        self.assertEqual(task.convertToInteger("5"), 5)
-    def test_6(self):
-        self.assertEqual(task.convertToInteger("6"), 6)
-    def test_7(self):
-        self.assertEqual(task.convertToInteger("7"), 7)
-    def test_8(self):
-        self.assertEqual(task.convertToInteger("8"), 8)
-    def test_9(self):
-        self.assertEqual(task.convertToInteger("9"), 9)
-    def test_0(self):
-        self.assertIsNone(task.convertToInteger("0"))
-    def test_a(self):
-        self.assertIsNone(task.convertToInteger("a"))
-
-class ReplaceNumericWithInteger(unittest.TestCase):
-    def test_a(self):
-        self.assertEqual(
-            task.replaceNumericWithInteger("three9three"),
-            "393"
-        )
-    def test_b(self):
-        self.assertEqual(
-            task.replaceNumericWithInteger("5hxseven7jhsqlsftml"),
-            "5hx77jhsqlsftml"
-        )
-    def test_c(self):
-        self.assertEqual(
-            task.replaceNumericWithInteger("xtwone3four"),
-            "x2ne34",
-        )
-
 class ConvertLineToNumber(unittest.TestCase):
     def test_a(self):
         self.assertEqual(
