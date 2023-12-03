9552f2c2bd5404c055fce3f0342fc72a26c01470
Dominic Carrington <carringtondominic@gmail.com>
2023-12-03 10:53:54 +0000

    tests: update 3.x tests to be better checks of the function. Previous the function could just int()

--- 2023/3.1/test.py
+++ 2023/3.1/test.py
@@ -46,23 +46,43 @@ class ExampleTest(unittest.TestCase):
 class FindNumberTest(unittest.TestCase):
     def test_0(self):
         self.assertEqual(
-            task.findNumber("1234", 0),
+            task.findNumber("1234.5678", 0),
             1234
         )
     def test_1(self):
         self.assertEqual(
-            task.findNumber("1234", 1),
+            task.findNumber("1234.5678", 1),
             1234
         )
     def test_2(self):
         self.assertEqual(
-            task.findNumber("1234", 2),
+            task.findNumber("1234.5678", 2),
             1234
         )
     def test_3(self):
         self.assertEqual(
-            task.findNumber("1234", 3),
+            task.findNumber("1234.5678", 3),
             1234
+        )
+    def test_5(self):
+        self.assertEqual(
+            task.findNumber("1234.5678", 5),
+            5678
+        )
+    def test_6(self):
+        self.assertEqual(
+            task.findNumber("1234.5678", 6),
+            5678
+        )
+    def test_7(self):
+        self.assertEqual(
+            task.findNumber("1234.5678", 7),
+            5678
+        )
+    def test_8(self):
+        self.assertEqual(
+            task.findNumber("1234.5678", 8),
+            5678
         )
 
 if __name__ == "__main__":
