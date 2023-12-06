c3369600db1cabac2a76b3315c8558701804536e
Dominic Carrington <dominic@watchthedot.com>
2023-12-06 15:51:21 +0000

    [2023][5.2] Complete
    The brute force approach has worked!
    There must be a better method since this took 1hr 25mins

--- 2023/5.2/test.py
+++ 2023/5.2/test.py
@@ -2,67 +2,16 @@ import task
 import unittest
 
 class ExampleTest(unittest.TestCase):
-    def test_79(self):
+    def test_1(self):
         self.assertEqual(
-            task.parseFile("test_inputs/seed-to-soil.txt", 79),
-            81
+            task.parseSeedRange((1, 1)),
+            2048657150
         )
-    def test_14(self):
+    def test_2(self):
         self.assertEqual(
-            task.parseFile("test_inputs/seed-to-soil.txt", 14),
-            14
+            task.parseSeedRange((1, 2)),
+            2048657150
         )
-    def test_52(self):
-        self.assertEqual(
-            task.parseFile("test_inputs/seed-to-soil.txt", 52),
-            54
-        )
-    def test_53(self):
-        self.assertEqual(
-            task.parseFile("test_inputs/seed-to-soil.txt", 53),
-            55
-        )
-    def test_55(self):
-        self.assertEqual(
-            task.parseFile("test_inputs/seed-to-soil.txt", 55),
-            57
-        )
-    def test_13(self):
-        self.assertEqual(
-            task.parseFile("test_inputs/seed-to-soil.txt", 13),
-            13
-        ) 
-class ExampleRevTest(unittest.TestCase):
-    def test_79(self):
-        self.assertEqual(
-            task.revParseFile("test_inputs/seed-to-soil.txt", 81),
-            79
-        )
-    def test_14(self):
-        self.assertEqual(
-            task.revParseFile("test_inputs/seed-to-soil.txt", 14),
-            14
-        )
-    def test_52(self):
-        self.assertEqual(
-            task.revParseFile("test_inputs/seed-to-soil.txt", 54),
-            52
-        )
-    def test_53(self):
-        self.assertEqual(
-            task.revParseFile("test_inputs/seed-to-soil.txt", 55),
-            53
-        )
-    def test_55(self):
-        self.assertEqual(
-            task.revParseFile("test_inputs/seed-to-soil.txt", 57),
-            55
-        )
-    def test_13(self):
-        self.assertEqual(
-            task.revParseFile("test_inputs/seed-to-soil.txt", 13),
-            13
-        ) 
 
 if __name__ == "__main__":
     unittest.main();
