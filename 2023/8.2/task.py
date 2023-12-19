0f6da883d20249c57b31660b0ea0c4d84e1bd9d5
Dominic Carrington <carringtondominic@gmail.com>
2023-12-19 21:03:38 +0000

    [2023][8.2] Complete
    Code works but it reaches the max integer of Python and overflows. The lcm function in numpy returns negative.
    Wolfram Alpha returns the correct solution with all the loop counts.
    Confirming a few hypothesis with the data included!

--- 2023/8.2/task.py
+++ 2023/8.2/task.py
@@ -1,6 +1,7 @@
 # https://adventofcode.com/2023/day/8
 import os
 import re
+import numpy as np
 
 def parseFile(lines: list):
     directions = lines.pop(0).strip()
@@ -21,16 +22,38 @@ def parseFile(lines: list):
         if node.endswith('A'):
             current_nodes.append(node)
 
-    count = 0
-    while not all([n.endswith('Z') for n in current_nodes]):
-        direction = directions[count % len(directions)]
-        direction_index = 0 if direction == "L" else 1
+    loop_counts = []
+    for node in current_nodes:
+        current_node = str(node)
+        count = 0
+        while not current_node.endswith('Z'):
+            direction = directions[count % len(directions)]
+            direction_index = 0 if direction == "L" else 1
+            current_node = graph[current_node][direction_index]
+            count += 1
+        end_node = current_node
+        last_count = count
+        loop_counts.append(count)
 
-        for i in range(len(current_nodes)):
-            current_nodes[i] = graph[current_nodes[i]][direction_index]
+        print("{} -> {} in {}".format(node, current_node, count))
+        for i in range(1, 4):
+            direction = directions[count % len(directions)]
+            direction_index = 0 if direction == "L" else 1
+            current_node = graph[current_node][direction_index]
+            count += 1
+            while current_node != end_node:
+                direction = directions[count % len(directions)]
+                direction_index = 0 if direction == "L" else 1
+                current_node = graph[current_node][direction_index]
+                count += 1
 
-        count += 1
-    return count
+            loop_count = count - last_count
+            last_count = count
+
+            print("  Loop {} in {}".format(i, loop_count))
+
+    print(loop_counts)
+    return np.lcm.reduce(loop_counts)
 
 def main():
     dir = os.path.dirname(__file__)
