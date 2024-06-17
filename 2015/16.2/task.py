dc430c6b1e1605feae9bdcecf2d06d3c048f9597
Dominic Carrington <carringtondominic@gmail.com>
2024-06-17 22:48:39 +0000

    [2015][16.2] Correct

---
+++ 2015/16.2/task.py
@@ -0,0 +1,60 @@
+# https://adventofcode.com/2015/day/16
+import os
+import re
+import math
+
+MFCSAM_OUTPUT = {
+    "children": 3,
+    "cats": 7,
+    "samoyeds": 2,
+    "pomeranians": 3,
+    "akitas": 0,
+    "vizslas": 0,
+    "goldfish": 5,
+    "trees": 3,
+    "cars": 2,
+    "perfumes": 1,
+}
+
+GREATER_THAN_RULES = ['cats', 'trees']
+LESS_THAN_RULES = ['pomeranians', 'goldfish']
+
+def parseLine(line: str) -> tuple[int, bool]:
+    matches = re.match(r"Sue (\d+): (.+)$", line)
+    if matches is None:
+        raise ValueError
+
+    number = int(matches.group(1))
+    categories = [c.strip() for c in matches.group(2).split(',')]
+
+    for c in categories:
+        m = re.match(r"(\w+): (\d+)", c)
+        category_name = m.group(1)
+        count = int(m.group(2))
+
+        if category_name in GREATER_THAN_RULES:
+            if MFCSAM_OUTPUT[category_name] >= count:
+                return (number, False)
+        elif category_name in LESS_THAN_RULES:
+            if MFCSAM_OUTPUT[category_name] <= count:
+                return (number, False)
+        else:
+            if MFCSAM_OUTPUT[category_name] != count:
+                return (number, False)
+    return (number, True)
+
+def parseFile(contents: str) -> int:
+    matches = []
+    for line in contents.splitlines():
+        (sue_no, is_match) = parseLine(line)
+        if is_match:
+            matches.append(sue_no)
+    return matches
+
+def main():
+    dir = os.path.dirname(__file__)
+    with open(dir + '/input.txt') as f:
+        print(parseFile(f.read().strip()))
+
+if __name__ == "__main__":
+    main()
